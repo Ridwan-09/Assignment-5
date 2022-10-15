b6f5a699ea9d21ffe636ae0264b1be49795825e2
Ridwan Salim <ridwan.salim@gmail.com>
2022-10-15 20:20:39 +0600

    imac

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
-document.getElementById('').addEventListener('click', function(){
+function memoryButton(memoryId, chooseMemory){
+    const buyMemory = document.getElementById('extra-memory-cost');
+    const buyMemoryInput = buyMemory.innerText;
+    if (chooseMemory == true){
+        newPrice = $0;
+    }
+    else if(chooseMemory == false){
+        newPrice = $180;
+    }
+    return buyMemoryInput;
+}
 
+
+document.getElementById('8GB-memory').addEventListener('click', function(){
+    memoryButton('8GB', true);
+});
+
+document.getElementById('16GB-memory').addEventListener('click', function(){
+    memoryButton('16GB', false);
 });
