941a556ad53654821e837e06e2c0049169b91b8e
Ridwan Salim <ridwan.salim@gmail.com>
2022-10-15 23:23:49 +0600

    imac

--- js/app.js
+++ js/app.js
@@ -9,6 +9,20 @@ function memoryCost(isAdd){
     }
 }
 
+// for storage 
+function storageCost(isCost){
+    const unifiedStorage = document.getElementById('extra-storage-cost');
+    if(isCost == true){
+        unifiedStorage.innerText = 0;
+    }
+    if(isCost == true){
+        unifiedStorage.innerText = 100; 
+    }
+    else if(isCost == false){
+        unifiedStorage.innerText = 180;
+    }
+}
+
 // memory events
 document.getElementById('8GB-memory').addEventListener('click', function(){
     memoryCost(true);
@@ -16,4 +30,17 @@ document.getElementById('8GB-memory').addEventListener('click', function(){
 
 document.getElementById('16GB-memory').addEventListener('click', function(){
     memoryCost(false);
+});
+
+// storage events
+document.getElementById('256GB-storage').addEventListener('click', function(){
+    storageCost(true);
+});
+
+document.getElementById('512GB-storage').addEventListener('click', function(){
+    storageCost(false);
+});
+
+document.getElementById('1TB-storage').addEventListener('click', function(){
+    storageCost(false);
 });
