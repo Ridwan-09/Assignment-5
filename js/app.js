e126d4c1dc161cfa2b5b2120789d102fb69b21b0
Ridwan Salim <ridwan.salim@gmail.com>
2022-10-16 14:50:23 +0600

    imac

--- js/app.js
+++ js/app.js
@@ -1,51 +1,36 @@
 // memory
-function memoryBtn(memory){
+function memoryBtn(price){
     const unifiedMemory = document.getElementById('extra-memory-cost');
-    const unifiedMemoryText = unifiedMemory.innerText;
-    if(memory == true){
-        unifiedMemory.innerText = 0;
-    }
-    else {
-        unifiedMemory.innerText = 180;
-    }
-  
+    const unifiedMemoryText = unifiedMemory.innerText = price;
+    return unifiedMemoryText;
 }
 
 
 // storage
 function storageBtn(storage){
     const ssdStorage = document.getElementById('extra-storage-cost');
-    const ssdStorageText = ssdStorage.innerText;
-    if(storage == true){
-        ssdStorage.innerText = 0;
-    }
-    else if(storage == true){
-        ssdStorage.innerText = 100;
-    }
-    else{
-        ssdStorage.innerText = 180;
-    }
-    
+    const ssdStorageText = ssdStorage.innerText = storage;
+    return ssdStorageText;
 }
 
 // memory events
 document.getElementById('8GB-memory').addEventListener('click', function(){
-    memoryBtn(true);
+    memoryBtn('0');
 });
 
 document.getElementById('16GB-memory').addEventListener('click', function(){
-    memoryBtn(false);
+    memoryBtn('180');
 });
 
 // storage events
 document.getElementById('256GB-storage').addEventListener('click', function(){
-    storageBtn(true);
+    storageBtn('0');
 });
 
 document.getElementById('512GB-storage').addEventListener('click', function(){
-    storageBtn(true);
+    storageBtn('100');
 });
 
 document.getElementById('1TB-storage').addEventListener('click', function(){
-    storageBtn(false);
+    storageBtn('180');
 });
