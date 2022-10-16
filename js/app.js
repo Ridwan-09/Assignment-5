ee180cfe254833e23d4c78e5554bcb80e1d11a8b
Ridwan Salim <ridwan.salim@gmail.com>
2022-10-16 09:41:11 +0600

    imac

--- js/app.js
+++ js/app.js
@@ -1,68 +1,52 @@
 // for memory 
-function memoryCost(isAdd){
+function memoryCost(){
     const unifiedMemory = document.getElementById('extra-memory-cost');
-    if(isAdd == true){
-        unifiedMemory.innerText = 0;
-    }
-    else{
-        unifiedMemory.innerText = 180;
-    }
+    const unifiedMemoryText = unifiedMemory.innerText;
+    return unifiedMemoryText;
 }
 
 // for storage 
-function storageCost(isCost){
-    const unifiedStorage = document.getElementById('extra-storage-cost');
-    if(isCost == true){
-        unifiedStorage.innerText = 0;
-    }
-    else if(isCost == false){
-        unifiedStorage.innerText = 100;
-    }
-    else if(isCost == false){
-        unifiedStorage.innerText = 180;
-    }
-}
+// function storageCost(isCost){
+    
+// }
 
-// for delivery
-function deliveryCost(isDelivered){
-    const deliverOption = document.getElementById('delivery-charge');
-    if(isDelivered == true){
-        deliverOption.innerText = 0;
-    }
-    else{
-        deliverOption.innerText = 20;
-    }
-}
+// // for delivery
+// function deliveryCost(isDelivered){
+    
+// }
+
+// calculation
+
 
 
 // memory events
 document.getElementById('8GB-memory').addEventListener('click', function(){
-    memoryCost(true);
+    memoryCost('unifiedMemoryText');
 });
 
 document.getElementById('16GB-memory').addEventListener('click', function(){
-    memoryCost(false);
+    memoryCost('unifiedMemoryText');
 });
 
-// storage events
-document.getElementById('256GB-storage').addEventListener('click', function(){
-    storageCost(true);
-});
+// // storage events
+// document.getElementById('256GB-storage').addEventListener('click', function(){
+//     storageCost(true);
+// });
 
-document.getElementById('512GB-storage').addEventListener('click', function(){
-    storageCost(false);
-});
+// document.getElementById('512GB-storage').addEventListener('click', function(){
+//     storageCost(false);
+// });
 
-document.getElementById('1TB-storage').addEventListener('click', function(){
-    storageCost(false);
-});
+// document.getElementById('1TB-storage').addEventListener('click', function(){
+//     storageCost(false);
+// });
 
 
-// delivery option events
-document.getElementById('free-delivery').addEventListener('click', function(){
-    deliveryCost(true);
-});
+// // delivery option events
+// document.getElementById('free-delivery').addEventListener('click', function(){
+//     deliveryCost(true);
+// });
 
-document.getElementById('pay-delivery').addEventListener('click', function(){
-    deliveryCost(false);
-});
+// document.getElementById('pay-delivery').addEventListener('click', function(){
+//     deliveryCost(false);
+// });
